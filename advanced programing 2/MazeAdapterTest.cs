970f2035c45e24385e10cf8842a34e8e34b502e5
orie50 <turht50@gmail.com>
2017-04-08 22:06:48 +0300

    Add files via upload

--- advanced programing 2/MazeAdapterTest.cs
+++ advanced programing 2/MazeAdapterTest.cs
@@ -6,9 +6,9 @@ using SearchAlgorithmsLib;
 
 namespace advanced_programing_2
 {
-    internal class MazeAdapterTest
+    class MazeAdapterTest
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             int col = 10;
             int row = 10;
@@ -20,15 +20,37 @@ namespace advanced_programing_2
             Console.WriteLine(mazeString);
 
             ISearchable<Position> adapter = new MazeAdapter(maze);
-            ISearcher<Position> searcher = new BestFirstSearch<Position>();
-            ISolution<Position> sol = searcher.Search(adapter);
+            ISearcher<Position> BfsSearcher = new BestFirstSearch<Position>();
+            ISolution<Position> sol = BfsSearcher.Search(adapter);
+
+            int BfsNumOfStases = BfsSearcher.GetNumberOfNodesEvaluated();
 
             StringBuilder sb = new StringBuilder(mazeString);
 
             foreach (State<Position> s in sol)
+            {
                 sb[s.Data.Row * (col + 2) + s.Data.Col] = '$';
+            }
 
             Console.WriteLine(sb.ToString());
+
+            ISearcher<Position> DfsSearcher = new DepthFirstSearch<Position>();
+            sol = DfsSearcher.Search(adapter);
+
+            int DfsNumOfStases = DfsSearcher.GetNumberOfNodesEvaluated();
+
+            //Console.WriteLine("number of BFS states:" + BfsNumOfStases);
+            //Console.WriteLine("number of DFS states:" + DfsNumOfStases);
+
+            sb = new StringBuilder(mazeString);
+
+            foreach (State<Position> s in sol)
+            {
+                sb[s.Data.Row * (col + 2) + s.Data.Col] = '$';
+            }
+
+            Console.WriteLine(sb.ToString());
+            
             Console.Read();
         }
     }
