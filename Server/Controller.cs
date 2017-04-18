b87e0266dab0ec12264d80c2845f96af57398000
adielcahana <adielcahana@gmail.com>
2017-04-18 05:27:17 +0300

    Start command

--- Server/Controller.cs
+++ Server/Controller.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using MazeLib;
 
 namespace Server
 {
     class Controller
     {
         private Dictionary<string, ICommand> commands;
+        private Dictionary<string, Game> games;
+
         private MazeModel model;
         public Controller()
         {
             //model = new Model();
             commands = new Dictionary<string, ICommand>();
+            games = new Dictionary<string, Game>();
             //commands.Add("generate", new GenerateMazeCommand(model));
             // more commands...
         }
@@ -24,6 +30,24 @@ namespace Server
             ICommand command = commands[commandKey];
             return null;
             //return command.Execute(args);
+        }
+
+        public string NewGame(String name, Maze maze, TcpClient player1)
+        {
+            games.Add(name, new Game(name, maze, player1));
+            return "waiting for player2";
+        }
+
+        public string JoinGame(String name, TcpClient player2)
+        {
+            Game game;
+            if (games.TryGetValue(name, out game))
+            {
+                game.AddPlayer(player2);
+                game.Start();
+                return "starting game";
+            }
+            return "the name: " + name + "does not exist";
         }
     }
 }
---
+++ Server/Game.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using MazeLib;
+
+namespace Server
+{
+    class Game
+    {
+        private TcpClient _player1;
+        private TcpClient _player2;
+        private Position _player1Position;
+        private Position _player2Position;
+        private string _name;
+        private Maze _maze;
+
+        public Game(string name, Maze maze, TcpClient player1)
+        {
+            _maze = maze;
+            _name = name;
+            _player1 = player1;
+            _player1Position = maze.InitialPos;
+
+        }
+
+        public void AddPlayer(TcpClient player2)
+        {
+            _player2 = player2;
+            _player2Position = _maze.InitialPos;
+        }
+
+        public bool isRunning()
+        {
+            Position goal = _maze.GoalPos;
+            return !(_player1Position.Equals(goal) || _player2Position.Equals(goal));
+        }
+
+        private void PlayOneTurn(TcpClient player, Position playerPosition)
+        {
+            using (NetworkStream stream = player.GetStream())
+            using (StreamReader reader = new StreamReader(stream))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                string move = reader.ReadLine();
+            }
+        }
+
+        public void Start()
+        {
+            new Task(() =>
+            {
+                {
+                    while (isRunning)
+                    {
+                        PlayOneTurn(_player1, _player1Position);
+                        PlayOneTurn(_player2, _player2Position);
+                    }
+                    if (_player1Position.Equals(_maze.GoalPos))
+                    {
+                        using (NetworkStream stream = _player1.GetStream())
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            writer.WriteLine("you won");
+                            writer.Flush();
+                        }
+                    }
+                    else
+                    {
+                        using (NetworkStream stream = _player2.GetStream())
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            writer.WriteLine("you won");
+                            writer.Flush();
+                        }
+                    }
+                }
+                _player1.Close();
+                _player2.Close();
+            }).Start();
+        }
+    }
+}
