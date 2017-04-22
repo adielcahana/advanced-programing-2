f08f0be8891ea2ed1da7fb5a957fb35e18bc8c49
adielcahana <adielcahana@gmail.com>
2017-04-22 23:18:04 +0300

    convetion fixing

--- Server/Commands/Close.cs
+++ Server/Commands/Close.cs
@@ -2,31 +2,18 @@
 
 namespace Server.Commands
 {
-    /// <summary>
-    /// close game command.
-    /// </summary>
-    /// <seealso cref="Server.ICommand" />
     class Close : ICommand
     {
+        /// <summary>
+        /// The model
+        /// </summary>
         private IModel _model;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Close"/> class.
-        /// </summary>
-        /// <param name="model">The model.</param>
         public Close(IModel model)
         {
             this._model = model;
         }
 
-        /// <summary>
-        /// exectue the command according the arguments.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
-        /// <param name="client">The client that send the command.</param>
-        /// <returns>
-        /// the answer to the client
-        /// </returns>
         public string Execute(string[] args, TcpClient client = null)
         {
             if (args.Length != 1)
