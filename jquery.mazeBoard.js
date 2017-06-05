a77b5895c874c3e36f14140bcc830758aaaec677
adielcahana <adielcahana@gmail.com>
2017-06-05 21:43:31 +0300

    mazeboard completion

--- jquery.mazeBoard.js
+++ jquery.mazeBoard.js
@@ -5,12 +5,14 @@
         startCol,
         exitRow,
         exitCol,
-        playerImage,
+        playerRight,
+        layeLeft,
         exitImage,
         wallImg,
         isEnabled) {
+
         class mazeBoard {
-            constructor(mazeData, startRow, startCol, exitRow, exitCol, playerImage, exitImage, wallImg, isEnabled, canvas) {
+            constructor(mazeData, startRow, startCol, exitRow, exitCol, playerRight, playerLeft, exitImage, wallImg, isEnabled, canvas) {
                 this._data = mazeData;
                 this._startRow = startRow;
                 this._startCol = startCol;
@@ -18,7 +20,9 @@
                 this._exitCol = exitCol;
                 this._playerRow = startRow;
                 this._playerCol = startCol;
-                this._playerImg = playerImage;
+                this._playerLeft = playerLeft;
+                this._playerRight = playerRight;
+                this._player = playerRight;
                 this._exitImg = exitImage;
                 this._wallImg = wallImg;
                 this._isEnabled = isEnabled;
@@ -36,7 +40,7 @@
                 for (var i = 0; i < rows; i++) {
                     for (var j = 0; j < cols; j++) {
                         if (i == this._startRow && j == this._startCol) {
-                            ctx.drawImage(this._playerImg, width * j, height * i, width, height);
+                            ctx.drawImage(this._playerRight, width * j, height * i, width, height);
                         } else if (i == this._exitRow && j == this._exitCol) {
                             ctx.drawImage(this._exitImg, width * j, height * i, width, height);
                         } else if (this._data[i][j] == 1) {
@@ -73,7 +77,11 @@
                     return false;
                 }  
             }
-
+            makeMove(direction) {
+                if (this._isEnabled == true) {
+                    this.move(direction);
+                }
+            }
 
             move(direction) {
                 var ctx = this._canvas.getContext("2d");
@@ -81,43 +89,33 @@
                 var cols = this._data[0].length;
                 var width = this._canvas.width / cols;
                 var height = this._canvas.height / rows;
+                switch (direction) {
+                case "right":
+                    this._player = this._playerRight;
+                    break;
+                case "left":
+                    this._player = this._playerLeft;
+                    break;
+               default:
+                    break;
+                }
                 if (this.isValidMove(direction)) {
                     switch (direction) {
                     case "up":
                         ctx.fillRect(width * this._playerCol, height * this._playerRow, width, height);
                         this._playerRow -= 1;
-                        ctx.drawImage(this._playerImg,
-                            width * this._playerCol,
-                            height * this._playerRow,
-                            width,
-                            height);
                         break;
                     case "down":
                         ctx.fillRect(width * this._playerCol, height * this._playerRow, width, height);
                         this._playerRow += 1;
-                        ctx.drawImage(this._playerImg,
-                            width * this._playerCol,
-                            height * this._playerRow,
-                            width,
-                            height);
                         break;
                     case "right":
                         ctx.fillRect(width * this._playerCol, height * this._playerRow, width, height);
                         this._playerCol += 1;
-                        ctx.drawImage(this._playerImg,
-                            width * this._playerCol,
-                            height * this._playerRow,
-                            width,
-                            height);
                         break;
                     case "left":
                         ctx.fillRect(width * this._playerCol, height * this._playerRow, width, height);
                         this._playerCol -= 1;
-                        ctx.drawImage(this._playerImg,
-                            width * this._playerCol,
-                            height * this._playerRow,
-                            width,
-                            height);
                         break;
                         default:
                             var e = new Error();
@@ -132,6 +130,39 @@
                             height);
                     }
                 }
+                ctx.drawImage(this._player,
+                    width * this._playerCol,
+                    height * this._playerRow,
+                    width,
+                    height);
+            }
+
+            solve(board, solution) {
+                board._isEnabled = false;
+                var i = 0;
+                function stage() {
+                    switch (solution[i]) {
+                        case 0: //left 
+                            board.move("left");
+                            break;
+                        case 1:
+                            board.move("right");
+                            break;
+                        case 2:
+                            board.move("up");
+                            break;
+                        case 3:
+                            board.move("down");
+                            break;
+                    }
+                    if (i < solution.length) {
+                        i++;
+                    } else {
+                        board._isEnabled = true;
+                        clearInterval();
+                    }
+                }
+                setInterval(stage, 500);
             }
         }
 
@@ -140,7 +171,8 @@
             startCol,
             exitRow,
             exitCol,
-            playerImage,
+            playerRight,
+            layeLeft,
             exitImage,
             wallImg,
             isEnabled,
