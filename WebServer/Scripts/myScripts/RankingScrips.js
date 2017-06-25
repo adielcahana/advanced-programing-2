f178a073e9310279e5207a7dcdea5936fbcf998c
orie50 <turht50@gmail.com>
2017-06-25 18:11:59 +0300

    add ranking page

---
+++ WebServer/Scripts/myScripts/RankingScrips.js
@@ -0,0 +1,23 @@
+﻿var ViewModel = function () {
+    var self = this; // make 'this' available to subfunctions or closures
+
+    self.Ranks = ko.observableArray(); // enables data binding
+    self.rank = ko.observableArray();
+    var RanksUri = '/api/Ranks/';
+    function getRanks() {
+        $.getJSON(RanksUri).done(function (data) {
+            data.sort(function (a, b) { return (b.GamesWon - b.GamesLost) - (a.GamesWon - a.GamesLost) });
+            rank = 1;
+            data.forEach(function (user) {
+                user.Rank = rank;
+                rank = rank + 1;
+            }
+
+            );
+            self.Ranks(data);
+        });
+    }
+    // Fetch the initial data
+    getRanks();
+};
+ko.applyBindings(new ViewModel());
